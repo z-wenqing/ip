f328778ff36418d781e3a93de55fbd442390d6b2
z-wenqing <zhangwenqing0824@gmail.com>
2024-01-31 21:29:06 +0800

    Level-2

--- src/main/java/Wqchat.java
+++ src/main/java/Wqchat.java
@@ -4,6 +4,9 @@ public class Wqchat {
         System.out.println("____________________________________________________________");
     }
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         printLine();
         System.out.println("Hello! I'm Wqchat");
         System.out.println("What can I do for you?");
@@ -14,11 +17,28 @@ public class Wqchat {
         line = in.nextLine();
 
         while (!line.equals("bye")) {
-            printLine();
-            System.out.println(line);
-            printLine();
+            if (!line.equals("list")) {
+                printLine();
+                System.out.println("added: " + line);
+                printLine();
+
+                tasks[taskCount] = line;
+                taskCount++;
+            } else {
+                printLine();
+                for (int i = 0; i < 100; i++) {
+                    if (tasks[i] != null) {
+                        System.out.print(i + 1);
+                        System.out.println(". " + tasks[i]);
+                    }
+                }
+                printLine();
+
+            }
+
             line = in.nextLine();
         }
+
         printLine();
         System.out.println("Bye. Hope to see you again soon!");
         printLine();
