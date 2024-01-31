6a9a0c28e8a493d3e602667dd8e3621781c19613
z-wenqing <zhangwenqing0824@gmail.com>
2024-01-31 21:06:36 +0800

    Level-1

--- src/main/java/Wqchat.java
+++ src/main/java/Wqchat.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
 public class Wqchat {
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,11 +10,28 @@ public class Wqchat {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
-        System.out.println("____________________________________________________________");
+        //System.out.println("____________________________________________________________");
+        printLine();
         System.out.println("Hello! I'm wqchat");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        //System.out.println("____________________________________________________________");
+        printLine();
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        if (line.equals("bye")) {
+            printLine();
+            System.out.println("Bye. Hope to see you again soon!");
+            printLine();
+        } else {
+            printLine();
+            System.out.println(line);
+            printLine();
+        }
+
+
+
     }
 }
